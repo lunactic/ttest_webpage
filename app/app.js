a3c7f98cc28ada1e6224974474fa2407f9eb2a25
lunactic <lunactic@gmail.com>
2015-12-18 11:47:12 +0100

    initial code commit

---
+++ app/app.js
@@ -0,0 +1,28 @@
+var myApp = angular.module('myApp', [])
+    .controller('MyCtrl', MyCtrl)
+    .filter('to_trusted', ['$sce', function ($sce) {
+        return function (text) {
+            return $sce.trustAsHtml(text);
+        };
+    }]);
+
+
+function MyCtrl($scope, $http) {
+    $scope.formData;
+    $scope.formType = "raw";
+    $scope.result;
+    $scope.processForm = function () {
+        var data = {
+            "inputs": {
+                "input": $scope.formData,
+                "inputType": $scope.formType
+            },
+            "highlighter": {}
+        }
+
+        $http.post('http://192.168.56.101/services/statistics/cmplst', data).
+            success(function (response) {
+                $scope.result = response.data
+            });
+    }
+}
